164be391f98f9a6bb71276c0f53265138a48e0d8
bin.xu <bin.xu@spreadtrum.com>
2012-10-12 16:30:05 -0400

    Merge modem_reboot from 2.3.5
    Change-Id: I868acb2425bdf8c7a14b42a55f25fe01a5fb7d76

---
+++ kylew/modem_reboot/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	main.c modem_boot.c packet.c crc16.c
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils 
+
+LOCAL_MODULE := modem_reboot
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
