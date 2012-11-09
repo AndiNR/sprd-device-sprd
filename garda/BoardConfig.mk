e592608184d32bb2d5f61182e69bf980f979b7f8
dale.hou <dale.hou@spreadtrum.com>
2012-11-09 12:52:33 +0800

    Add garda for SS, base on sp8825ea
    Change-Id: I07245dcba807a591dfcb20eccc16bf851aae0d3e

---
+++ garda/BoardConfig.mk
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOARD_PLATFORM := sc8825
+TARGET_BOOTLOADER_BOARD_NAME := garda
+
+TARGET_VLX_ENABLE ?= true
+
+# config u-boot
+TARGET_NO_BOOTLOADER := false
+ifeq ($(TARGET_VLX_ENABLE), true)
+UBOOT_DEFCONFIG := garda
+else
+UBOOT_DEFCONFIG := garda_native
+endif
+
+# config kernel
+TARGET_NO_KERNEL := false
+ifeq ($(TARGET_VLX_ENABLE), true)
+KERNEL_DEFCONFIG := garda-vlx_defconfig
+else
+KERNEL_DEFCONFIG := garda-native_defconfig
+endif
+USES_UNCOMPRESSED_KERNEL := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+
+# use default init.rc
+TARGET_PROVIDES_INIT_RC := false
+
+# ext4 partition layout
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 300000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2500000000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 150000000
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+USE_OPENGL_RENDERER := true
+
+USE_CAMERA_STUB := true
+
+BOARD_USES_GENERIC_AUDIO := false
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_USE_VETH := true
