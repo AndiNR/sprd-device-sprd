50a1932d2438b277dc699cd8573ce052b0552ac8
dale.hou <dale.hou@spreadtrum.com>
2012-11-12 16:48:09 +0800

    Fix sp8825ea 4.1 bootup
    Change-Id: I72839c9699da6cb05523103a929bc2a7bcfb1b24

--- sp8825ea/base.mk
+++ sp8825ea/base.mk
@@ -13,11 +13,15 @@ PRODUCT_PACKAGES := \
 	$(MALI)
 
 PRODUCT_COPY_FILES := \
-	$(BOARDDIR)/init.sp8825.rc:root/init.sp8825.rc \
-	$(BOARDDIR)/init.sp8825.usb.rc:root/init.sp8825.usb.rc \
-	$(BOARDDIR)/ueventd.sp8825.rc:root/ueventd.sp8825.rc \
-	$(BOARDDIR)/fstab.sp8825:root/fstab.sp8825 \
+	$(BOARDDIR)/init.rc:root/init.rc \
+	$(BOARDDIR)/init.sc8825.rc:root/init.sc8825.rc \
+	$(BOARDDIR)/init.sc8825.usb.rc:root/init.sc8825.usb.rc \
+	$(BOARDDIR)/ueventd.sc8825.rc:root/ueventd.sc8825.rc \
+	$(BOARDDIR)/fstab.sc8825:root/fstab.sc8825 \
 	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab \
+        $(BOARDDIR)/hw_params/tiny_hw.xml:system/etc/tiny_hw.xml \
+        $(BOARDDIR)/hw_params/codec_pga.xml:system/etc/codec_pga.xml \
+        $(BOARDDIR)/hw_params/audio_para:system/etc/audio_para \
 	device/sprd/common/libs/mali/egl.cfg:system/lib/egl/egl.cfg \
 	device/sprd/common/libs/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	device/sprd/common/res/media/media_codecs.xml:system/etc/media_codecs.xml \
