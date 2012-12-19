53b75fd272c26f6f1a4c1dca391ce9c7cc9a9070
allen.zhang <allen.zhang@spreadtrum.com>
2012-12-20 04:46:54 +0800

    Bug#000 add sp7702 native board
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I66a2f77cbc4752921fef0b9e695c93870563b0ab

---
+++ sp7702/BoardConfig.mk
@@ -0,0 +1,88 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOARD_PLATFORM := sc8810
+TARGET_BOOTLOADER_BOARD_NAME := sp7702
+
+TARGET_VLX_ENABLE ?= true
+
+# config u-boot
+TARGET_NO_BOOTLOADER := false
+ifeq ($(TARGET_VLX_ENABLE), true)
+UBOOT_DEFCONFIG := sp7702
+else
+UBOOT_DEFCONFIG := sp7702_native
+endif
+
+# config kernel
+TARGET_NO_KERNEL := false
+ifeq ($(TARGET_VLX_ENABLE), true)
+KERNEL_DEFCONFIG := sp7702-vlx_defconfig
+else
+KERNEL_DEFCONFIG := sp7702-native_defconfig
+endif
+USES_UNCOMPRESSED_KERNEL := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+
+# use default init.rc
+TARGET_PROVIDES_INIT_RC := true
+
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_BLUETOOTH_BCM := false
+# board specific modules
+BOARD_USES_TINYALSA_AUDIO := true
+BOARD_USE_VETH := true
+BOARD_SPRD_RIL := true
+BOARD_SAMSUNG_RIL := false
+TARGET_RECOVERY_UI_LIB := librecovery_ui_sp8810
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+
+
+
+
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION     := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/etc/wifi/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/etc/wifi/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/etc/wifi/fw_bcmdhd_apsta.bin"
+USE_CAMERA_STUB := true
+
+BOARD_USES_GENERIC_AUDIO := false
+USE_BOOT_AT_DIAG := true
+
+#fm
+BOARD_HAVE_FM_BCM := true
+
+####################!!!! DONT'T FOLLOW THIS WAY TO CUSTOMIZE  !!!###############
+BOARD_CMMB_HW ?= siano
+
+ifeq ($(BOARD_CMMB_HW), mxd)
+KERNEL_DEFCONFIG := sp8810ea-vlx-mxd_defconfig
+CMMB_MXDID       := 0266
+endif
+################################################################################
---
+++ sp7702/base.mk
@@ -0,0 +1,92 @@
+
+MALI := libUMP libEGL_mali.so libGLESv1_CM_mali.so libGLESv2_mali.so libMali.so ump.ko mali.ko
+
+MXD_CMMB_PLAYER := mxdcmmbplayer.apk  \
+		   libiwcmmbdev.so libiwcmmb_jni.so  \
+		   libiwmbbms_jni.so libiwmbbms.so libiwmbbms_tel.so \
+		   libiwuam.so  libmxdcmmb.so libplayEngine.so mxdcmmbserver mxdid.id libiwcmmb_jni_demon.so libiwcmmbdev_demon.so mxdcmmbtest.apk
+PRODUCT_PROPERTY_OVERRIDES :=
+
+# original apps copied from generic_no_telephony.mk
+PRODUCT_PACKAGES := \
+	DeskClock \
+	Bluetooth \
+	Calculator \
+	Calendar \
+	CertInstaller \
+	DrmProvider \
+	Email \
+	Exchange2 \
+	Gallery2 \
+	InputDevices \
+	LatinIME \
+	Launcher2 \
+	Music \
+	MusicFX \
+	Provision \
+	QuickSearchBox \
+	SystemUI \
+	CalendarProvider \
+	bluetooth-health \
+	hciconfig \
+	hcitool \
+	hcidump \
+	FMPlayer \
+	bttest\
+	hostapd \
+	wpa_supplicant.conf \
+    SoundRecorder
+
+PRODUCT_PACKAGES += \
+	nvitemd \
+	charge \
+	vcharged \
+	modemd \
+	calibration_init \
+	gralloc.$(TARGET_PLATFORM) \
+	hwcomposer.$(TARGET_PLATFORM) \
+	camera.$(TARGET_PLATFORM) \
+	lights.$(TARGET_PLATFORM) \
+	audio.primary.$(TARGET_PLATFORM) \
+	tinymix \
+	sensors.$(TARGET_BOARD)  \
+	fm.$(TARGET_PLATFORM)  \
+	libmbbms_tel_jni.so\
+	$(MALI)
+
+ifeq ($(BOARD_HAVE_FM_BCM),true)
+BRCMFM := \
+	FmDaemon \
+	FmTest
+
+PRODUCT_PACKAGES += $(BRCMFM)
+endif
+
+ifeq ($(BOARD_CMMB_HW), mxd)
+PRODUCT_PACKAGES += $(MXD_CMMB_PLAYER)
+else
+PRODUCT_PACKAGES += $(SIANOMTV)
+endif
+
+PRODUCT_COPY_FILES := \
+	$(BOARDDIR)/init.rc:root/init.rc \
+	$(BOARDDIR)/init.sp8810.rc:root/init.sp8810.rc \
+	$(BOARDDIR)/init.sp8810.usb.rc:root/init.sp8810.usb.rc \
+	$(BOARDDIR)/ueventd.sp8810.rc:root/ueventd.sp8810.rc \
+	$(BOARDDIR)/fstab.sp8810:root/fstab.sp8810 \
+	$(BOARDDIR)/vold.fstab:system/etc/vold.fstab \
+        $(BOARDDIR)/hw_params/tiny_hw.xml:system/etc/tiny_hw.xml \
+        $(BOARDDIR)/hw_params/codec_pga.xml:system/etc/codec_pga.xml \
+        $(BOARDDIR)/hw_params/audio_para:system/etc/audio_para \
+        $(BOARDDIR)/features/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        $(BOARDDIR)/features/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	device/sprd/common/libs/mali/egl.cfg:system/lib/egl/egl.cfg \
+	device/sprd/common/libs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+	device/sprd/common/res/media/media_codecs.xml:system/etc/media_codecs.xml \
+	device/sprd/common/res/media/media_profiles.xml:system/etc/media_profiles.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	device/sprd/common/res/apn/apns-conf.xml:system/etc/apns-conf.xml
+
+BOARD_WLAN_DEVICE_REV       := bcm4330_b2
+$(call inherit-product, $(BOARDDIR)/../common/apps/engineeringmodel/module.mk)
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
