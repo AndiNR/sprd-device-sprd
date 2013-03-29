e20d51423f769098af60d33b21f2ca75c9fc2ed4
xielj <xielj@spreadst.com>
2013-03-29 12:43:31 +0800

    bug#142108 SP7710_dualsim boot animation is not full-screen display
     [bug number  ]142108
     [root cause  ]resoltion of the issue
     [changes     ]change the resolution
     [side effects] boot animation is not full-screen display
     [reviewers   ]xiajing@spreadst.com
    
    Change-Id: I56aa9ef286bdf84d33438c334f3240b377b4d617

--- sp7710ga/prod_sp7710ga.mk
+++ sp7710ga/prod_sp7710ga.mk
@@ -75,7 +75,7 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-256-dalvik-heap.mk)
 # include classified configs
 $(call inherit-product, $(BOARDDIR)/base.mk)
 $(call inherit-product, $(BOARDDIR)/proprietories.mk)
-$(call inherit-product, device/sprd/common/res/boot/boot_res.mk)
+$(call inherit-product, vendor/sprd/operator/cucc/specA/res/boot/boot_res.mk)
 
 # include standard configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
