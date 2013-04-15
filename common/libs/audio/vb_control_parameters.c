3045257f6d2d1329123b816d4a56bbe3a2fb2b5c
jian.chen <jian.chen@spreadtrum.com>
2013-04-15 11:16:36 +0800

    Bug#00 need to wait for codec here before call connected include headset
    [bug number  ] 00
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I4ec3e6eab234dd4ffb8c8086ae2b1a8e998c3531

--- common/libs/audio/vb_control_parameters.c
+++ common/libs/audio/vb_control_parameters.c
@@ -466,10 +466,9 @@ static void SetCall_ModePara(struct tiny_audio_device *adev,paras_mode_gain_t *m
         }
     }
     /*
-        we need to wait for codec here,maybe driver needs to fix this problem.
-        We don't need to wait when in headset,because there is 200ms opening headset.
+        we need to wait for codec here before call connected, maybe driver needs to fix this problem.
     */
-    if((!switch_headset) && (android_cur_device) && (!adev->call_connected)){
+    if(!adev->call_connected){
         usleep(1000*100);
     }
 	ALOGW("%s successfully, device: earpice(%s), headphone(%s), speaker(%s), Main_Mic(%s), Back_Mic(%s), hp_mic(%s) devices(0x%x)"
