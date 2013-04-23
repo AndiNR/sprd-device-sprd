64b393deb3caf173074f6fb97e1e7ba95904bc62
xiaowei.luo <xiaowei.luo@spreadtrum.com>
2013-04-23 10:51:21 +0800

    modify Android.mk file, utest_vsp only for 8830
    Change-Id: I183f819f7f65c0122d4e38fcca7bb1769627a578

--- common/tools/utest/utest_vsp/Android.mk
+++ common/tools/utest/utest_vsp/Android.mk
@@ -3,9 +3,12 @@ ifneq ($(shell ls -d vendor/sprd/proprietories-source 2>/dev/null),)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc8830)
 
 include $(LOCAL_PATH)/Android_vsp_enc.mk
 #include $(LOCAL_PATH)/Android_vsp_mpeg4dec.mk
 #include $(LOCAL_PATH)/Android_vsp_h264dec.mk
+
+endif
 
 endif
