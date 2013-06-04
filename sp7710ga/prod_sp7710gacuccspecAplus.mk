0a62f9c321900ec3a33374dd3bbf42a54f82b340
huibin.mao <huibin.mao@spreadtrum.com>
2013-06-04 21:12:01 +0800

    bug#171867 Modify baidu map
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: Idc5f29b181cb907a067a2cf10f04e4c2b6d4c8fb

--- sp7710ga/prod_sp7710gacuccspecAplus.mk
+++ sp7710ga/prod_sp7710gacuccspecAplus.mk
@@ -76,9 +76,10 @@ PRODUCT_PACKAGES += \
 	SogouInput_31143_android_oem_zhanxun_20130509.apk \
 	SohuNewsClient_v3.3_factory_zhanxun_6230.apk \
 	Wostore.apk \
-	UnicomClient.apk \
+	Unicomclient_SP7710-4.1.apk \
 	wo116114.apk \
-	AIMail_Android_V375a_orange.apk
+	AIMail_Android_V375a_orange.apk \
+	baidumap_v5_0_0.apk
 
 # packages files
 PRODUCT_PACKAGES += \
