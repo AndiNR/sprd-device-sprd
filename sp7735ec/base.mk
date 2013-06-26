292f45525f78854e4618644af3b49d3858f323aa
tony.liu <tony.liu@spreadtrum.com>
2013-06-26 15:20:00 +0800

    bug#182332 add aac decoder
    Change-Id: I7221d80cd7895472af37e633b24b0e0756babec6

--- sp7735ec/base.mk
+++ sp7735ec/base.mk
@@ -55,7 +55,8 @@ PRODUCT_PACKAGES += \
     libstagefright_sprd_mpeg4dec \
     libstagefright_sprd_h264dec	\
     libstagefright_sprd_h264enc	\
-    libstagefright_sprd_vpxdec 
+    libstagefright_sprd_vpxdec \
+    libstagefright_sprd_aacdec
 
 # prebuild files
 PRODUCT_PACKAGES += \
