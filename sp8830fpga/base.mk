e6eecadb5f24af5a3750e904fe6d056fa1b79cf4
guobin.zhu <guobin.zhu@spreadtrum.com>
2013-07-01 11:12:28 +0800

    bug#172938 add headset kl file
    [bug number ]
    [root cause ]
    [changes ]
    [side effects]
    [reviewers ]
    
    Change-Id: Icfcca97884adbd1072cb41209aa6d68aacde3970

--- sp8830fpga/base.mk
+++ sp8830fpga/base.mk
@@ -64,6 +64,7 @@ PRODUCT_COPY_FILES := \
         $(BOARDDIR)/hw_params/codec_pga.xml:system/etc/codec_pga.xml \
 	$(BOARDDIR)/hw_params/audio_hw.xml:system/etc/audio_hw.xml \
         $(BOARDDIR)/hw_params/audio_para:system/etc/audio_para \
+	$(BOARDDIR)/headset-keyboard.kl:system/usr/keylayout/headset-keyboard.kl \
 	device/sprd/common/libs/mali/egl.cfg:system/lib/egl/egl.cfg \
 	device/sprd/common/libs/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	device/sprd/common/res/media/media_codecs.xml:system/etc/media_codecs.xml \
