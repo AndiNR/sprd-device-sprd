7ce70f6de606f54ea2341643ed3425419edfabb4
ke.wang <ke.wang@spreadtrum.com>
2013-07-10 16:31:24 +0800

    Bug#177893 Synchronous wait config veth done when setup data call
    [bug number ]
    [root cause ]
    [changes ]set property "done" as the data on/off script complete condition
    [side effects]
    [reviewers ]
    
    Change-Id: I80a130258e95d2e73dc7fadd1115dc3a544c861f

--- shark0ea/scripts/ext_data.sh
+++ shark0ea/scripts/ext_data.sh
@@ -5,7 +5,10 @@ if [ "$1" = "-u" ]; then
 	ifconfig $ifname
 	ifname=`getprop sys.data.noarp`
 	ip $ifname
+	setprop sys.ifconfig.up done
+	setprop sys.data.noarp done
 elif [ "$1" = "-d" ]; then
 	ifname=`getprop sys.ifconfig.down`
 	ifconfig $ifname
+	setprop sys.ifconfig.down done
 fi
