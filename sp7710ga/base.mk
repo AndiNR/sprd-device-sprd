35298b0a215490b775dd5592d957bae598cb0c24
Haili Tian(田海立) <haili.tian@spreadtrum.com>
2013-08-05 13:29:35 +0800

    BUG#186644 add feature microphone and screen.portrait
    Change-Id: I4692dd5a5b85300ff669671661a3b1e0d90f982c

--- sp7710ga/base.mk
+++ sp7710ga/base.mk
@@ -116,6 +116,8 @@ PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
 	frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml \
+	frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml \
+	frameworks/native/data/etc/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
